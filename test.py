e6542adda25c0cfcd6522f0c0c54fc302cf60ad9
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-06-18 16:42:36 +0200

    updated test.py with example sequence file

--- test.py
+++ test.py
@@ -2,26 +2,18 @@ import pystarcode
 
 # parse file
 seq_list = []
-with open('iPCR_rep1_filtered.txt','r') as f :
+with open('example_seqs.txt','r') as f :
     for line in f :
         seq_list.append(line.strip('\n'))
 
 # invoke starcode
-clusteralg = "components"
+clusteralg = "mp"
 canonical_counts, d = pystarcode.starcode(seq_list,2,clusteralg=clusteralg)
 
 # print counts output
-# for key, l in canonical_counts.iteritems() :
-    # print '%s -> %d'%(key,l)
+for key, l in canonical_counts.iteritems() :
+    print '%s -> %d'%(key,l)
 
 # print output
-# for key, l in d.iteritems() :
-    # print '%s -> %s'%(key,l)
-
-with open('test-%s.txt.true'%(clusteralg), 'r') as f :
-    for line in f :
-        canonical, counts = line.split('\t')
-        counts = int(counts)
-        if canonical_counts[canonical] != counts :
-            print "Discrepancy between counts of %s"%(canonical)
-            break
+for key, l in d.iteritems() :
+    print '%s -> %s'%(key,l)

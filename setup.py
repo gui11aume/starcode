31eb8a30f282b77512a57f1f7c450af667b0e43b
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-06-29 12:03:07 +0200

    Moves new files to their folders. Adds pystarcode tests to main test routine.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 module1 = Extension('pystarcode',
       define_macros = [('MAJOR_VERSION', '1'), ('MINOR_VERSION', '0')],
       include_dirs = ['src/'],
-      sources = ['pystarcode.c', 'src/starcode.c', 'src/trie.c'],
+      sources = ['src/pystarcode.c', 'src/starcode.c', 'src/trie.c'],
       extra_compile_args = ['-std=c99'])
 
 setup(name = 'pystarcode',
--- src/pystarcode.c
+++ src/pystarcode.c
@@ -1,8 +1,8 @@
 #include <Python.h>
 #include <stdlib.h>
 #include <string.h>
-#include "src/starcode.h"
-#include "src/trie.h"
+#include "starcode.h"
+#include "trie.h"
 
 #define MAX_STR_LENGTH 2048
 

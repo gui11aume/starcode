ece37f2618d7efd2db6aa0da36049a8411ec472c
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-09-05 14:33:25 -0700

    Wrote a little short test in the extratests.sh

--- test/extratests.sh
+++ test/extratests.sh
@@ -15,3 +15,13 @@
 ../starcode --sphere --non-redundant test_file_spheres.fastq 2>/dev/null | \
    tr -d "\r\n" | \
    grep -q "@seq1/1AGGGCTTACAAGTATAGGCC+BBBBBBBBBBBBBBBBBBBB"
+
+# Testing per-read output format, alone
+../starcode test_file_spheres.fastq --per-read --seq-id 2> /dev/null | \
+   tr -d "\r\n" | \
+   grep -q "AGGGCTTACAAGTATAGGCCAGGGCTTACAAGTATAGGCCAGGGCTTACAAGTATAGGCCAGGGCTTACAAGTATAGGCCAGGGCTTACAAGTATAGGCCAGGGCTTACAAGTATAGGCCAGGGCTTACAAGTATAGGCAAGGGCTTACAAGTATAGGCA"
+
+# Testing per-read output format, with seq-id
+../starcode test_file_spheres.fastq --per-read --seq-id 2> /dev/null | \
+   tr -d "\r\n" | \
+   grep -q "GGGCTTACAAGTATAGGCC[[:space:]]1AGGGCTTACAAGTATAGGCC[[:space:]]2AGGGCTTACAAGTATAGGCC[[:space:]]3AGGGCTTACAAGTATAGGCC[[:space:]]4AGGGCTTACAAGTATAGGCC[[:space:]]5AGGGCTTACAAGTATAGGCC[[:space:]]6AGGGCTTACAAGTATAGGCA[[:space:]]7AGGGCTTACAAGTATAGGCA[[:space:]]8"
